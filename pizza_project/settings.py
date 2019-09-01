a60eec253623cb0fab3bf78b1b06b19798710c46
rusrom <rusrom@guyfawkes.33mail.com>
2019-09-02 00:47:43 +0300

    Set langguage to russian and time for Kiev

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
