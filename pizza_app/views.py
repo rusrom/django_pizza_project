91abe31f838d7b958dcef538721dfab0c65fd251
rusrom <rusrom@guyfawkes.33mail.com>
2019-09-02 15:12:47 +0300

    Start implementing Login/Logout/Signup

--- pizza_app/views.py
+++ pizza_app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
+    return redirect(pizzashop)
+
+@login_required(login_url='/login/')
+def pizzashop(request):
     return render(request, 'pizza_app/home.html')
+
+def signup(request):
+    return render(request, 'pizza_app/signup.html')
--- pizza_project/urls.py
+++ pizza_project/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.contrib.auth import views as auth_views
+
 from pizza_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'pizza_app/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^pizzashop/$', views.pizzashop, name='pizzashop'),
 ]
